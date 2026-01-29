a8819ee088c5aace1207aba938766a07976489af
AbhishekSingh2511 <abhishek.singh@thestarterlabs.com>
2026-01-29 15:05:58 +0530

    FirstPush1

--- components/layout/sign-out-button.tsx
+++ components/layout/sign-out-button.tsx
@@ -1,15 +1,15 @@
-\"use client\";
+"use client";
 
-import { signOut } from \"next-auth/react\";
+import { signOut } from "next-auth/react";
 
-import { Button } from \"@/components/ui/button\";
+import { Button } from "@/components/ui/button";
 
 export default function SignOutButton() {
   return (
     <Button
-      type=\"button\"
-      variant=\"ghost\"
-      onClick={() => signOut({ callbackUrl: \"/\" })}
+      type="button"
+      variant="ghost"
+      onClick={() => signOut({ callbackUrl: "/" })}
     >
       Sign out
     </Button>
